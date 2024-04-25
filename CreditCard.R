db730de3c01ae4c9c4b2dc1ef443771e9e58b2f5
JiuqinWei <36885304+JiuqinWei@users.noreply.github.com>
2024-04-25 16:07:55 -0400

    New

---
+++ CreditCard.R
@@ -0,0 +1,3 @@
+data = read.csv("/Users/jacobwei/Documents/GitHub/STAT675-Final-Project/application_record.csv") # Applicants Data
+record = read.csv("/Users/jacobwei/Documents/GitHub/STAT675-Final-Project/credit_record.csv") # Credit Record Data
+
